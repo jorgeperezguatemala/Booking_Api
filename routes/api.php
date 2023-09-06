e0f64d097a1fc0bc0b6fd01a93738cff63aaecab
Joel Barrera <jbarreraperaza1111@gmail.com>
2023-09-06 12:33:25 -0600

    :sparkle:modificacion de las rutas

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route:: get('/guests', [GuestController:: class, 'index']);
+Route::post('/guests',[GuestController::class, 'store']);
+Route::put('/guests/{id}',[GuestController::class, 'update']);
+
+Route:: get('/rooms', [RoomController:: class, 'index']);
+Route::post('/rooms',[RoomController::class, 'store']);
+Route::put('/rooms/{id}',[RoomController::class, 'update']);
+
+Route:: get('/bookings', [BookingController:: class, 'index']);
+Route::post('/bookings',[BookingController::class, 'store']);
+Route::put('/bookings/{id}',[BookingController::class, 'update']);
